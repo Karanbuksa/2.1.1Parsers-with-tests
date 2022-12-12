353e8635516906325edf3f90a9196593a857aa92
Artyom Bekkuzhinov <bekkygi27@gmail.com>
2022-12-12 23:06:17 +0500

    1.5.2 XML-JSON Parser MVN

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,25 +5,30 @@ import com.opencsv.CSVReader;
 import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
         String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
         String fileName = "data.csv";
         List<Employee> list = parseCSV(columnMapping, fileName);
         String json = listToJson(list);
-        writeString(json);
+        writeString(json, "data.json");
+        List<Employee> list2 = parseXML("data.xml");
+        String json2 = listToJson(list2);
+        writeString(json2, "data2.json");
     }
 
     public static List<Employee> parseCSV(String[] columnMapping, String fileName) {
@@ -46,41 +51,50 @@ public class Main {
         Type listType = new TypeToken<List<Employee>>() {
         }.getType();
         GsonBuilder gsonBuilder = new GsonBuilder();
+        gsonBuilder.setPrettyPrinting();
         Gson gson = gsonBuilder.create();
         return gson.toJson(list, listType);
     }
 
-    public static void writeString(String string) {
-        ArrayList<Integer> indexes = new ArrayList<>();
-        for (int index = string.indexOf('['); index >= 0; index = string.indexOf('[', index + 1)) {
-            indexes.add(index);
-        }
-        for (int index = string.indexOf('{'); index >= 0; index = string.indexOf('{', index + 1)) {
-            indexes.add(index);
-        }
-        for (int index = string.indexOf(','); index >= 0; index = string.indexOf(',', index + 1)) {
-            indexes.add(index + 1);
-        }
-        for (int index = string.indexOf(']'); index >= 0; index = string.indexOf(']', index + 1)) {
-            indexes.add(index);
-        }
-        for (int index = string.indexOf('}'); index >= 0; index = string.indexOf('}', index + 1)) {
-            indexes.add(index);
-        }
+    public static void writeString(String string, String name) {
 
-        List<Integer> ind = indexes.stream()
-                .sorted(Comparator.reverseOrder()).toList();
-        StringBuilder stringBuilder = new StringBuilder(string);
-        for (int i = 0; i < indexes.size() - 1; i++) {
-            stringBuilder.insert(ind.get(i), "\n");
-        }
-
-        try (FileWriter fileWriter = new FileWriter("data.json")) {
-            fileWriter.write(stringBuilder.toString());
+        try (FileWriter fileWriter = new FileWriter(name)) {
+            fileWriter.write(string);
             fileWriter.flush();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-}
 
+    public static List<Employee> parseXML(String string) throws ParserConfigurationException, IOException, SAXException {
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.parse(new File(string));
+        Node root = doc.getDocumentElement();
+        List<Employee> employeeList = new ArrayList<>();
+        NodeList nodeList = root.getChildNodes();
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node node_ = nodeList.item(i);
+            if (Node.ELEMENT_NODE == node_.getNodeType()) {
+                Element element = (Element) node_;
+                NodeList contents = element.getChildNodes();
+                List<String> values = new ArrayList<>();
+                for (int a = 0; a < contents.getLength(); a++) {
+                    if (contents.item(a).getTextContent() != null && !contents.item(a).getTextContent().contains("\n"))
+                        values.add(contents.item(a).getTextContent());
+                }//TODO: Пожалуйста, проверьте это. Думаю, есть штатный способ заполнить объект
+                if (values.size() != 0) {
+                    Employee employee = new Employee();
+                    employee.id = Long.parseLong(values.get(0));
+                    employee.firstName = values.get(1);
+                    employee.lastName = values.get(2);
+                    employee.country = values.get(3);
+                    employee.age = Integer.parseInt(values.get(4));
+                    employeeList.add(employee);
+                }
+            }
+        }
+        return employeeList;
+    }
+}
