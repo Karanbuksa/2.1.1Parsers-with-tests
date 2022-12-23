5cfd14d08e62f8a63db753ac4f9270832a5b2e09
Artyom Bekkuzhinov <bekkygi27@gmail.com>
2022-12-23 12:31:50 +0500

    2.1.1 Final

--- src/test/java/ParsersTests.java
+++ src/test/java/ParsersTests.java
@@ -1,5 +1,6 @@
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.ParserConfigurationException;
@@ -7,21 +8,15 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasSize;
-
 public class ParsersTests {
     List<Employee> expectedEmployees = new ArrayList<>();
     @BeforeEach
     public void prepare() {
-
         expectedEmployees.add(new Employee(1, "John", "Smith", "USA", 25));
         expectedEmployees.add(new Employee(2, "Ivan", "Petrov", "RU", 23));
         expectedEmployees.add(new Employee(3, "David", "Tenant", "UK", 45));
         expectedEmployees.add(new Employee(4, "Math", "Smith", "UK", 38));
     }
-
 
     @Test
     public void parseXML_test() throws ParserConfigurationException, IOException, SAXException {
@@ -29,7 +24,7 @@ public class ParsersTests {
 
         List<Employee> employees = Main.parseXML(data);
 
-        assertThat(expectedEmployees.toString(), equalTo(employees.toString()));
+        Assertions.assertEquals(expectedEmployees.toString(), employees.toString());
 
     }
 
@@ -38,9 +33,10 @@ public class ParsersTests {
         String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
         String data = "src/test/resources/data.csv";
 
+
         List<Employee> employees = Main.parseCSV(columnMapping, data);
 
-        assertThat(expectedEmployees.toString(), equalTo(employees.toString()));
+        Assertions.assertEquals(expectedEmployees.toString(), employees.toString());
 
 
     }
@@ -49,9 +45,12 @@ public class ParsersTests {
     public void readString_test() {
         String data = "src/test/resources/data.json";
 
+
         List<Employee> employees = Main.jsonToList(Main.readString(data));
 
-        assertThat(expectedEmployees.toString(), equalTo(employees.toString()));
+        Assertions.assertEquals(expectedEmployees.toString(), employees.toString());
+
+
     }
 
     @Test
@@ -60,7 +59,6 @@ public class ParsersTests {
 
         List<Employee> employees = Main.jsonToList(Main.readString(data));
 
-        assertThat(employees, hasSize(4));
+        Assertions.assertEquals(employees.size(), 4);
     }
-
 }
